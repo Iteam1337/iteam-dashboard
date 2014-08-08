460819b32bee0e57af5ce239eac058c93f27939b
Radu Achim <radu.achim@iteam.se>
2014-08-08 10:31:42 +0200

    Project details
    Reported and planned default value to 0
    Dynamic databinding in project details #3

--- partial/project/details/details.js
+++ partial/project/details/details.js
@@ -1,10 +1,24 @@
 angular.module('iteam-dashboard').controller('ProjectDetailsCtrl', function ($scope, $stateParams, week) {
   'use strict';
   
-  $scope.activeSlide = $scope.activeWeek.index;
+  function bindUsers() {
+    week.getUsersForProject($scope.activeWeek.yearWeek, $stateParams.projectId)
+      .then(function (filtered) {
+        $scope.activeWeek.filteredUsers = filtered;
+      });
+  }
 
-  week.getUsersForProject($scope.activeWeek.yearWeek, $stateParams.projectId)
-    .then(function (filtered) {
-      $scope.users = filtered;
-    });
+  $scope.weekSelect = function (index) {
+    $scope.activeWeek = $scope.weeks[index];
+  };
+
+  // TOOD: parse from stateparams
+  $scope.activeSlider = $scope.weeks.indexOf($scope.activeWeek);
+
+  $scope.$watch('activeWeek', function (activeWeek) {
+    bindUsers();
+  }, true);
+
+  bindUsers();
+
 });
