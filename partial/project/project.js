141db2c54af8e2b4be2391f4cde0c2d43757cf9e
Radu Achim <radu.achim@iteam.se>
2014-08-08 12:06:10 +0200

    Dynamic databinding
    Moved weekSelect and watch activeWeek to rootScope and set the users and projects for a week on change

--- partial/project/project.js
+++ partial/project/project.js
@@ -1,17 +1,7 @@
 angular.module('iteam-dashboard').controller('ProjectCtrl', function ($scope, week) {
   'use strict';
 
-  $scope.weekSelect = function (index) {
-    $scope.activeWeek = $scope.weeks[index];
-  };
-
   // TOOD: parse from stateparams
   $scope.activeSlider = $scope.weeks.indexOf($scope.activeWeek);
-
-  $scope.$watch('activeWeek', function (activeWeek) {
-    activeWeek.projects = week.getProjects(activeWeek.yearWeek);
-  }, true);
-
-  $scope.activeWeek.projects = week.getProjects($scope.activeWeek.yearWeek);
 
 });
