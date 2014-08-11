7d39b559dda3e3599078fcdb240b5e8d56211946
Dennis Pettersson <mail@dennispettersson.se>
2014-08-11 10:01:38 +0200

    Added eveyones profile color from iteam.se

--- service/colors.js
+++ service/colors.js
@@ -1,23 +1,92 @@
 angular.module('iteam-dashboard').service('colors', function () {
   'use strict';
+  function hue(string) {
+    return Math.round(string.split('').reduce(function (numb, character, index) {
+      numb = (numb * character.charCodeAt(0) * (index + 0.7));
+      return numb;
+    }, 1) % 360);
+  }
+  function component(c) {
+    var hex = c.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }
+  function hsbToHEX(h) {
+    h = Math.max(1, Math.min(360, h)) / 60;
+    var s = saturation;
+    var b = brightness;
+    var i = Math.floor(h);
+    var f = h - i;
+    var p = b * (1 - s);
+    var q = b * (1 - s * f);
+    var t = b * (1 - s * (1 - f));
+    var R, G, B;
+    switch (i) {
+       case 0: R = b; G = t; B = p; break;
+       case 1: R = q; G = b; B = p; break;
+       case 2: R = p; G = b; B = t; break;
+       case 3: R = p; G = q; B = b; break;
+       case 4: R = t; G = p; B = b; break;
+      default: R = b; G = p; B = q;
+    }
+    return '#' +
+      component(Math.round(R * 255)) +
+      component(Math.round(G * 255)) +
+      component(Math.round(B * 255));
+  }
+  function generateColor(string) {
+    return hsbToHEX(hue(string));
+  }
+  function sanitizeValue(val) {
+    return Math.max(1, Math.min(100, val)) / 100;
+  }
+
+  var saturation = sanitizeValue(94);
+  var brightness = sanitizeValue(90);
 
   function Colors () {
-    this.users =  ['abo',     'acr',     'cln',     'dpn',     'jgn',     'jok',  'mln',     'ram',     'rln',     'jbn'];
-    this.colors = ['#4A87EE', '#66CC33', '#FFCC00', '#444444', '#34AADC', '#FAB', '#EF4E3A', '#8A6DE9', '#50DC82', '#F0B840'];
+    this.users =  {
+      // Must be a proper HEX code.
+      'acr': '#07AFCA',
+      'abo': '#000',
+      'apn': '#918C8E',
+      'cln': '#001337',
+      'dpn': '#666',
+      'hrn': '#55ae3a',
+      'jbn': '#0ff1ce',
+      'jok': '#FAB',
+      'mrs': '#830309',
+      'ram': '#bada55',
+      'rfn': '#8ab362',
+      'rln': '#8fe991',
+      'sru': '#e63132',
+      'mln': '#BA1B0A'
+    };
   }
 
   Colors.prototype.getColor = function (user) {
-    var color = this.colors[this.users.indexOf(user.toLowerCase())] || '#000';
-    return color;
+    user = (user || '').toLowerCase();
+    var predefined = this.users[user];
+    if (!!predefined) {
+      return predefined;
+    }
+    return generateColor(user);
   };
 
   Colors.prototype.shade = function (color, percent) {
-    var num = parseInt(color.slice(1), 16);
+    function addTwice(string, value) {
+      return string + value + value;
+    }
+    color = color.slice(1);
+    var length = color.length;
+    if (length === 3) {
+      color = color.split().reduce(addTwice, '');
+    }
+    var num = parseInt(color, 16);
     var amount = Math.round(2.55 * percent);
     var R = (num >> 16) + amount;
     var G = (num >> 8 & 0x00FF) + amount;
     var B = (num & 0x0000FF) + amount;
-    return "#" +
+    return '#' +
            (0x1000000 +
              (Math.max(Math.min(R, 255), 0)) * 0x10000 +
              (Math.max(Math.min(G, 255), 0)) * 0x100 +
