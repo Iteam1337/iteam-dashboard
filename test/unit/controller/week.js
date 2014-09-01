69303ec3cb119613296d4d3d5172478cbf93ad12
Radu Achim <radu.achim@iteam.se>
2014-09-01 14:43:42 +0200

    Added tests to WeekCtrl

--- test/unit/controller/week.js
+++ test/unit/controller/week.js
@@ -1,17 +1,75 @@
 describe('WeekCtrl', function () {
 
-  var scope, ctrl;
+  var scope;
+  var ctrl;
+  var clock;
+  var state;
 
   beforeEach(function () {
+    state = {
+      go: sinon.spy()
+    };
+    clock = sinon.useFakeTimers(0, 'Date');
     module('iteam-dashboard');
     inject(function ($rootScope, $controller) {
       scope = $rootScope.$new();
-      ctrl = $controller('WeekCtrl', {$scope: scope});
+      ctrl = $controller('WeekCtrl', {
+        $scope: scope,
+        $state: state
+      });
     });
   });
 
-  xit('should have tests', function () {
-    
+  afterEach(function () {
+    clock.restore();
   });
 
+  describe('#ctor', function () {
+    it('sets the weeks array', function () {
+      expect(scope.weeks).to.be.instanceof(Array);
+      expect(scope.weeks[5]).to.eql({
+        yearWeek: '19701',
+        year: 1970,
+        week: 1,
+        delta: 0,
+        projects: {},
+        users: {}
+      });
+    });
+    it('sets the activeWeek', function () {
+      expect(scope.activeWeek).to.eql({
+        yearWeek: '19701',
+        year: 1970,
+        week: 1,
+        delta: 0,
+        projects: {},
+        users: {}
+      });
+    });
+  });
+
+  describe('#goTo', function () {
+    it('navigates to the parameter state', function () {
+      scope.goTo('foo');
+      expect(state.go)
+        .calledOnce
+        .calledWith('foo');
+    });
+  });
+
+  describe('#weekSelect', function () {
+    it('sets the active week having the given index', function () {
+      scope.weekSelect(6);
+      expect(scope.activeWeek).to.eql({
+        yearWeek: '19702',
+        year: 1970,
+        week: 2,
+        delta: 1,
+        projects: {},
+        users: {}
+      });
+    });
+  });
+
+  describe('activeWeek $watch', function () {});
 });
